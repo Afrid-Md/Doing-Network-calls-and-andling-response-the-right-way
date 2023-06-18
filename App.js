a6251fa4332a43c777d00f07cc0bc71358c92d57
Afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-06-18 18:49:03 +0530

    on reload fetching data

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Layout from "./components/layout/layout";
 import Products from "./components/shop/products";
 import Notification from "./components/ui/notification";
 import { useDispatch, useSelector } from "react-redux";
-import { uiActions } from "./components/store";
+import { cartActions, uiActions } from "./components/store";
 
 
 let isInitial = true;
@@ -21,7 +21,6 @@ function App() {
   const newCart = useSelector((state) => state.cart);
 
   useEffect(() => {
-
     const sendingData = async () => {
       dispatch(
         uiActions.showNotification({
@@ -69,6 +68,41 @@ function App() {
       );
     });
   }, [newCart, dispatch]);
+
+
+  window.onload=async()=>{
+    try{
+    let response = await fetch("https://e-commerce-website-60ee5-default-rtdb.firebaseio.com/cart.json", {
+      method : 'GET',
+    })
+
+    if(!response.ok){
+      throw new Error('retriving data from backend failed')
+    }
+    let responseData = await response.json();
+    console.log(responseData);
+
+    dispatch(cartActions.replaceCart({items : responseData.cartItems, updatedQuantity : responseData.totalQuantity}));
+
+    dispatch(
+      uiActions.showNotification({
+        status: "success",
+        title: "Succes",
+        message: "Sent cart data successfully",
+      })
+    );
+    }
+  catch(err){
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed",
+        })
+      );
+    }
+  }
+
 
   return (
     <React.Fragment>
